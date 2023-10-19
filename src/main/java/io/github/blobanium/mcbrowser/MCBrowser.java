703ede6f501e53e7c340fb8e0f1cf51a2344ea63
Blobanium <45613796+Blobanium@users.noreply.github.com>
2023-10-19 16:17:22 -0700

    Remove Keybind

--- src/main/java/io/github/blobanium/mcbrowser/MCBrowser.java
+++ src/main/java/io/github/blobanium/mcbrowser/MCBrowser.java
@@ -6,10 +6,7 @@ import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
-import org.lwjgl.glfw.GLFW;
 
 
 public class MCBrowser implements ClientModInitializer {
@@ -39,15 +36,10 @@ public class MCBrowser implements ClientModInitializer {
     }
     private static final MinecraftClient minecraft = MinecraftClient.getInstance();
 
-    // H key to open a BasicBrowser screen
-    public static final KeyBinding KEY_MAPPING = new KeyBinding(
-            "Open Basic Browser", InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_H, "key.categories.misc"
-    );
 
     public void onTick() {
         // Check if our key was pressed
-        if ((KEY_MAPPING.wasPressed() || requestOpen) && !(minecraft.currentScreen instanceof BrowserScreen)) {
+        if (requestOpen && !(minecraft.currentScreen instanceof BrowserScreen)) {
             //Display the web browser UI.
 
             minecraft.setScreen(new BrowserScreen(
