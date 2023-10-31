07893a42d3ef615a36241c47a8208f732fba0be6
Blobanium <45613796+Blobanium@users.noreply.github.com>
2023-10-30 18:11:46 -0700

    add notif toasts

--- src/main/java/io/github/blobanium/mcbrowser/feature/specialbutton/SpecialButtonAction.java
+++ src/main/java/io/github/blobanium/mcbrowser/feature/specialbutton/SpecialButtonAction.java
@@ -6,6 +6,10 @@ import com.google.gson.JsonObject;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.MinecraftVersion;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.toast.SystemToast;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableTextContent;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
@@ -13,13 +17,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Map;
 
 public class SpecialButtonAction {
     public static String currentUrl = null;
 
     public static void downloadModrinthMod(){
+
         try {
+            MinecraftClient.getInstance().getToastManager().add(new SystemToast(SystemToast.Type.TUTORIAL_HINT, MutableText.of(new TranslatableTextContent("mcbrowser.moddownload.toast.downloadstarted", "Download Started", TranslatableTextContent.EMPTY_ARGUMENTS)), MutableText.of(new TranslatableTextContent("mcbrowser.moddownload.toast.downloadstarted.description", "Please wait while your mod downloads.", TranslatableTextContent.EMPTY_ARGUMENTS))));
+
             //Get the file from modrinth's API
             URL url = new URL("https://api.modrinth.com/v2/project/" + getModrinthSlugFromUrl(currentUrl) + "/version?game_versions=[%22" + MinecraftVersion.CURRENT.getName() + "%22]&loaders=[%22fabric%22]");
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
@@ -39,7 +45,10 @@ public class SpecialButtonAction {
             //get file
             URL downloadURL = new URL(file.get("url").getAsString());
             FileUtils.copyURLToFile(downloadURL, new File(FabricLoader.getInstance().getGameDir().toFile(), "mods/" + trimFileUrl(downloadURL.getFile())));
+
+            MinecraftClient.getInstance().getToastManager().add(new SystemToast(SystemToast.Type.TUTORIAL_HINT, MutableText.of(new TranslatableTextContent("mcbrowser.moddownload.toast.downloadcomplete", "Download Completed!", TranslatableTextContent.EMPTY_ARGUMENTS)), MutableText.of(new TranslatableTextContent("mcbrowser.moddownload.toast.downloadcomplete.description", "Restart your client for changes to take effect.", TranslatableTextContent.EMPTY_ARGUMENTS))));
         }catch (IOException e){
+            MinecraftClient.getInstance().getToastManager().add(new SystemToast(SystemToast.Type.TUTORIAL_HINT, MutableText.of(new TranslatableTextContent("mcbrowser.moddownload.toast.downloadfailed", "Download Failed", TranslatableTextContent.EMPTY_ARGUMENTS)), MutableText.of(new TranslatableTextContent( "mcbrowser.moddownload.toast.downloadfailed.description", "Check your logs for more info", TranslatableTextContent.EMPTY_ARGUMENTS))));
             e.printStackTrace();
         }
     }
