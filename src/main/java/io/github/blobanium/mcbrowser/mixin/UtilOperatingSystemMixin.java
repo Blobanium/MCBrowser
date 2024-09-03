169c9756fd1ab4f3a4087b794729c783a32b252d
Blobanium <45613796+Blobanium@users.noreply.github.com>
2024-09-03 16:12:41 -0700

    Fixed an issue where links do not open within MCBrowser.

--- src/main/java/io/github/blobanium/mcbrowser/mixin/UtilOperatingSystemMixin.java
+++ src/main/java/io/github/blobanium/mcbrowser/mixin/UtilOperatingSystemMixin.java
@@ -2,30 +2,23 @@ package io.github.blobanium.mcbrowser.mixin;
 
 import io.github.blobanium.mcbrowser.MCBrowser;
 import io.github.blobanium.mcbrowser.util.TabManager;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.util.Util;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.net.MalformedURLException;
 import java.net.URI;
 
-@Mixin(Screen.class)
-public class ScreenMixin {
-    @Shadow
-    @Nullable
-    protected MinecraftClient client;
-
-    @Redirect(method = "handleTextClick",
-              at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Util$OperatingSystem;open(Ljava/net/URI;)V"))
-    private void openLink(Util.OperatingSystem instance, URI uri) {
+@Mixin(Util.OperatingSystem.class)
+public class UtilOperatingSystemMixin {
+    @Inject(method = "open(Ljava/net/URI;)V", at = @At("HEAD"), cancellable = true)
+    private void open(URI uri, CallbackInfo ci){
         try {
             if (MCBrowser.getConfig().openLinkInBrowser) {
                 TabManager.openNewTab(uri.toURL().toString());
+                ci.cancel();
             }
         } catch (MalformedURLException e) {
             MCBrowser.LOGGER.error("Opening in browser. Failed to convert to URL", e);
